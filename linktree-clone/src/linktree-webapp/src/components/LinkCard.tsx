ad708abe0d61c5044ba6a21d94a581ad1b654e9a
imCertified <32418204+imCertified@users.noreply.github.com>
2023-08-04 18:44:52 -0400

    feat: initial commit of true button

--- linktree-clone/src/linktree-webapp/src/components/LinkCard.tsx
+++ linktree-clone/src/linktree-webapp/src/components/LinkCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Text, LinkOverlay, LinkBox } from "@chakra-ui/react";
+import { Button, Avatar, Text } from "@chakra-ui/react";
 
 interface LinkCardProps {
   displayText: string;
@@ -9,16 +9,32 @@ interface LinkCardProps {
 
 const LinkCard = ({ displayText, linkId, isExplicit, user }: LinkCardProps) => {
   return (
-    <LinkBox>
-    <LinkOverlay href={`https://api.linktree.portfolio.mannyserrano.com/redirect?user=${user}&linkId=${linkId}`}>
-      <Card direction="row" background={isExplicit ? 'red' : 'current'}>
-        <CardBody>
-          <Text>{displayText} - {linkId}</Text>
-        </CardBody>
-      </Card>
-    </LinkOverlay>
-    </LinkBox>
-
+    <>
+      {/* Width should eventually be bound to the container size */}
+      <Button
+        bg="red"
+        height="55px"
+        width="500px"
+        borderRadius="100px"
+        justifyContent={"space-between"}
+        onClick={() => {
+          if (isExplicit) {
+            alert("Explicit. Should be modal");
+          }
+          window.open(
+            `https://api.linktree.portfolio.mannyserrano.com/redirect?user=${user}&linkId=${linkId}`,
+            "_blank"
+          );
+        }}
+      >
+        <Avatar
+          size="md"
+          src="https://ugc.production.linktr.ee/Tn4NDgeFSEav8ZM3zzvZ_Screen%20Shot%202023-06-23%20at%208.47.05%20AM.png"
+        ></Avatar>
+        <Text color="white">{displayText}</Text>
+        <Text></Text>
+      </Button>
+    </>
   );
 };
 
