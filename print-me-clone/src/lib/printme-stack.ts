2035ede7c01a3f21c8a51013f5d19c377d3a1618
imCertified <32418204+imCertified@users.noreply.github.com>
2023-09-18 20:05:56 -0400

    feat: add bucket and URL generation function to deployment

--- print-me-clone/src/lib/printme-stack.ts
+++ print-me-clone/src/lib/printme-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 import * as r53 from 'aws-cdk-lib/aws-route53';
 import * as ddb from 'aws-cdk-lib/aws-dynamodb';
 import * as lda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as pythonLda from '@aws-cdk/aws-lambda-python-alpha';
@@ -34,6 +35,24 @@ export class PrintMeStack extends cdk.Stack {
       timeToLiveAttribute: 'ttl',
       billingMode: ddb.BillingMode.PAY_PER_REQUEST,
     });
+
+    const docBucket =  new s3.Bucket(this, 'DocBucket', {
+      bucketName: 'printme-doc-bucket',
+      blockPublicAccess: {
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true
+      },
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      lifecycleRules: [
+        {
+          enabled: true,
+          abortIncompleteMultipartUploadAfter: cdk.Duration.days(1),  // No permission is granted to even initiate MPUs, but just to be safe
+          expiration: cdk.Duration.days(7) // Matches the TTL of 7 days on DynamoDB objects
+        }
+      ]
+    })
 
     const domainLayer = new pythonLda.PythonLayerVersion(this, 'DomainLayer', {
       layerVersionName: 'PrintMeDomainLayer',
@@ -70,6 +89,22 @@ export class PrintMeStack extends cdk.Stack {
     });
     table.grantWriteData(newJobFunction);
 
+    const getUploadURLFunction = new pythonLda.PythonFunction(this, 'GetUploadURLFunction', {
+      functionName: 'PrintMeGetUploadURLFunction',
+      entry: 'lib/lambda/get_upload_url',
+      runtime: lda.Runtime.PYTHON_3_9,
+      architecture: lda.Architecture.X86_64,
+      index: 'app.py',
+      handler: 'lambda_handler',
+      environment: {
+        'TABLE_NAME': table.tableName,
+        'DOC_BUCKET': docBucket.bucketName
+      },
+      layers: [domainLayer, utilityLayer]
+    });
+    table.grantWriteData(getUploadURLFunction);
+    docBucket.grantPut(getUploadURLFunction);
+
     const apiCert = new acm.Certificate(this, 'APICertificate',
     {
       domainName: apiSubdomain,
@@ -90,9 +125,20 @@ export class PrintMeStack extends cdk.Stack {
     jobPath.addCorsPreflight({
       allowOrigins: ['*'],
       allowHeaders: ['*'],
-      allowMethods: ['GET', 'PUT']
+      allowMethods: ['GET', 'POST']
     });
-    jobPath.addMethod('PUT', new apigw.LambdaIntegration(newJobFunction));
+    jobPath.addMethod('POST', new apigw.LambdaIntegration(newJobFunction));
+    const jobProxyPath = jobPath.addResource('{jobId}');
+    const docPath = jobProxyPath.addResource('doc');
+    
+    const uploadPath = docPath.addResource('upload');
+    uploadPath.addCorsPreflight({
+      allowOrigins: ['*'],
+      allowHeaders: ['*'],
+      allowMethods: ['GET']
+    });
+    uploadPath.addMethod('POST', new apigw.LambdaIntegration(getUploadURLFunction))  // Path to get secure uploadURL
+    const docProxyPath = docPath.addResource('{docPath}')
     // const jobProxyPath = jobPath.addResource('{inviteId}');
     // jobProxyPath.addCorsPreflight({
     //   allowOrigins: ['*'],
